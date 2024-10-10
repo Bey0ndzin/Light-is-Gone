22d7677725992524ca5715f050a19d6810064b17
Pedro Nunes <zyxblackbeard@gmail.com>
2024-10-09 23:13:31 -0300

    Map advancements
    -Added more Lightning and map texture details

--- LightIsGone/Intermediate/Build/Mac/arm64/LightIsGoneEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "/Users/u22152/Documents/GitHub/Light-is-Gone/LightIsGone/Intermediate/Build/Mac/arm64/LightIsGoneEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
---
+++ LightIsGone/Intermediate/Build/Win64/x64/LightIsGoneEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/Pichau/Documents/GitHub/Light-is-Gone/LightIsGone/Intermediate/Build/Win64/x64/LightIsGoneEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
